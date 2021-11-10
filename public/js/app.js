e552247b4391ec61988e514ad6ddec85507601c5
jkondohoma <36901483+jkondohoma@users.noreply.github.com>
2021-11-10 13:15:10 -0600

    product list component created

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,18 @@
+//create a component using class
+class ProductList extends React.Component {
+  render() {
+    return (
+      <div className="ui unstackable items">
+        Hello, friend! I am a basic React component.
+      </div>
+    );
+  }
+}
 
+//create component using a function
+function HelloWord(){
+
+    return <p> Hello, World! </p>
+}
+
+ReactDOM.render( <ProductList />,document.getElementById('content'));
