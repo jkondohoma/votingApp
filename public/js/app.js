548d6c6ea3ff3d95135a5c208038902dd5329b23
jkondohoma <36901483+jkondohoma@users.noreply.github.com>
2021-11-10 16:54:55 -0600

    vote counters working

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,52 @@
 //create a component using class
+//parent
 class ProductList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      products: [],
+    };
+
+    this.handleProductUpVote = this.handleProductUpVote.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({ products: Seed.products });
+  }
+
   handleProductUpVote(productId) {
-    console.log(productId + " was upvoted.");
+    // “First, we use map() to traverse the products array.
+    // Importantly, map() returns a new array as
+    //  opposed to modifying the array this.state.products.”
+
+    // “Next, we check if the current product matches productId.
+    // If it does, we create a new object, copying over the properties from
+    //  the original product object. We then overwrite the votes property
+    //  on our new product object. We set it to the incremented vote count.
+    //   We do this using Object’s assign() method:”
+
+    // “If the current product is not the one specified
+    // by productId, we return it unmodified:”
+
+    const nextProducts = this.state.products.map((product) => {
+      if (product.id === productId) {
+        console.log(productId + " was upvoted.");
+        return Object.assign({}, product, {
+          votes: product.votes + 1,
+        });
+      } else {
+        return product;
+      }
+    });
+
+    this.setState({
+      products: nextProducts,
+    });
   }
 
   render() {
-    const products = Seed.products.sort((a, b) => b.votes - a.votes);
+    const products = this.state.products.sort((a, b) => b.votes - a.votes);
     const productComponents = products.map((product) => (
       <Product
         key={"product-" + product.id}
@@ -23,6 +64,8 @@ class ProductList extends React.Component {
     return <div className="ui unstackable items">{productComponents}</div>;
   }
 }
+
+//child
 class Product extends React.Component {
   constructor(props) {
     super(props);
