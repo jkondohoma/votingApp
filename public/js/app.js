23bf975683582c14f7bea6cbd71778de06e2b28e
jkondohoma <36901483+jkondohoma@users.noreply.github.com>
2021-11-10 13:47:02 -0600

    one product being displayed

--- public/js/app.js
+++ public/js/app.js
@@ -3,16 +3,40 @@ class ProductList extends React.Component {
   render() {
     return (
       <div className="ui unstackable items">
-        Hello, friend! I am a basic React component.
+       <Product/>
+      </div>
+    );
+  }
+}
+
+class Product extends React.Component {
+  render() {
+    return (
+      <div className="item">
+        <div className="image">
+          <img src="images/products/image-aqua.png" />
+        </div>
+
+        <div className="middle aligned content">
+          <div className="description">
+            <a> Fort Knight</a>
+            <p> Authentic renaissance actors, delivered in just two weeks.</p>
+          </div>
+        </div>
+
+        <div className="extra">
+          <span> Submitted by:</span>
+          <img className="ui avatar image" src="images/avatars/daniel.jpg" />
+        </div>
       </div>
     );
   }
 }
 
 //create component using a function
-function HelloWord(){
-
-    return <p> Hello, World! </p>
+function HelloWord() {
+  return <p> Hello, World! </p>;
 }
 
-ReactDOM.render( <ProductList />,document.getElementById('content'));
+ReactDOM.render(<ProductList />, document.getElementById("content"));
+ReactDOM.render();
