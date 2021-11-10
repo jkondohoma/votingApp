4d434e9b480f0960f4353acdd8fbd8dab79e385a
jkondohoma <36901483+jkondohoma@users.noreply.github.com>
2021-11-10 16:10:16 -0600

    on click event addd for clicking the upvote icon

--- public/js/app.js
+++ public/js/app.js
@@ -1,67 +1,66 @@
-
 //create a component using class
 class ProductList extends React.Component {
-    
-    render() {
-
-        const productComponents = Seed.products.map( (product) =>(
-            <Product
-
-            key={'product-' + product.id}
-            id={product.id}
-            title={product.title}
-            description={product.description}
-            url={product.url}
-            votes={product.votes}
-            submitterAvatarUrl={product.submitterAvatarUrl}
-            productImageUrl={product.productImageUrl}
-            
-            />
-        )
-
-        );
-
-      return (
-        <div className='ui unstackable items'>
-          {productComponents}
-        </div>
-      );
-    }
+  handleProductUpVote(productId) {
+    console.log(productId + " was upvoted.");
   }
+
+  render() {
+    const products = Seed.products.sort((a, b) => b.votes - a.votes);
+    const productComponents = products.map((product) => (
+      <Product
+        key={"product-" + product.id}
+        id={product.id}
+        title={product.title}
+        description={product.description}
+        url={product.url}
+        votes={product.votes}
+        submitterAvatarUrl={product.submitterAvatarUrl}
+        productImageUrl={product.productImageUrl}
+        onVote={this.handleProductUpVote}
+      />
+    ));
+
+    return <div className="ui unstackable items">{productComponents}</div>;
+  }
+}
 class Product extends React.Component {
-    render() {
-      return (
-        <div className='item'>
-          <div className='image'>
-            <img src={this.props.productImageUrl} />
+  constructor(props) {
+    super(props);
+    this.handleUpVote = this.handleUpVote.bind(this);
+  }
+
+  handleUpVote() {
+    this.props.onVote(this.props.id);
+  }
+  render() {
+    return (
+      <div className="item">
+        <div className="image">
+          <img src={this.props.productImageUrl} />
+        </div>
+        <div className="middle aligned content">
+          <div className="header">
+            <a onClick={this.handleUpVote}>
+              <i className="large caret up icon" />
+            </a>
+            {this.props.votes}
           </div>
-          <div className='middle aligned content'>
-            <div className='header'>
-              <a>
-                <i className='large caret up icon' />
-              </a>
-              {this.props.votes}
-            </div>
-            <div className='description'>
-              <a href={this.props.url}>
-                {this.props.title}
-              </a>
-              <p>
-                {this.props.description}
-              </p>
-            </div>
-            <div className='extra'>
-              <span>Submitted by:</span>
-              <img
-                className='ui avatar image'
-                src={this.props.submitterAvatarUrl}
-              />
-            </div>
+          <div className="description">
+            <a href={this.props.url}>{this.props.title}</a>
+            <p>{this.props.description}</p>
+          </div>
+          <div className="extra">
+            <span>Submitted by:</span>
+            <img
+              className="ui avatar image"
+              src={this.props.submitterAvatarUrl}
+            />
           </div>
         </div>
-      );
-    }
+      </div>
+    );
   }
+}
 
 //create component using a function
 function HelloWord() {
